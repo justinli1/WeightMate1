3a4cd840285cd2e7a4422400f1bdd87d6da75103
William G. <will.grand.wg@gmail.com>
2014-12-16 09:40:36 -0500

    Update CalculatorActivity.java

--- WeightMate/src/com/example/weightmate/CalculatorActivity.java
+++ WeightMate/src/com/example/weightmate/CalculatorActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RadioButton;
-
+// hi justin!
 public class CalculatorActivity extends ActionBarActivity {
 	public final static String EXTRA_BMR = "com.example.weightmate.BMR";
 	
